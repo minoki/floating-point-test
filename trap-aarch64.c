f41fd30bbbf471591ddbaa9f22b265037016080c
ARATA Mizuki <minorinoki@gmail.com>
2021-03-07 12:19:12 +0900

    Add a test for traps on AArch64

---
+++ trap-aarch64.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fenv.h>
+#include <stdint.h>
+
+#pragma STDC FENV_ACCESS ON
+
+// #if defined(__clang__)
+// __builtin_aarch64_get_fpcr and __builtin_aarch64_set_fpcr are not available on clang
+__attribute__((always_inline))
+uint64_t get_fpcr()
+{
+    uint64_t fpcr;
+    asm volatile("mrs %0, fpcr" : "=r"(fpcr));
+    return fpcr;
+}
+__attribute__((always_inline))
+void set_fpcr(uint64_t x)
+{
+    asm volatile("msr fpcr, %0" : : "r"(x));
+}
+// #endif
+
+int main(void)
+{
+    uint64_t fpcr = get_fpcr();
+    set_fpcr(fpcr | 0x9f00u); // IDE + IXE + UFE + OFE + DZE + IOE
+    fpcr = get_fpcr();
+    if (fpcr & 0x9f00u) {
+        puts("Traps are supported.");
+    } else {
+        puts("Traps are not supported.");
+    }
+    volatile double zero = 0.0, one = 1.0;
+    printf("1.0 / 0.0 = %g\n", one / zero);
+}
