6848e3be1d599d3619f46f0c623ec1e3cf627fa8
ARATA Mizuki <minorinoki@gmail.com>
2021-07-13 14:03:42 +0900

    Add fenv_access.c for testing #pragma STDC FENV_ACCESS

---
+++ fenv_access.c
@@ -0,0 +1,22 @@
+#include <fenv.h>
+#include <stdio.h>
+struct t {
+    double x, y;
+};
+struct t add(double a, double b)
+{
+    #pragma STDC FENV_ACCESS ON
+    fesetround(FE_UPWARD);
+    double x = a + b;
+    fesetround(FE_DOWNWARD);
+    double y = a + b;
+    fesetround(FE_TONEAREST);
+    return (struct t){.x = x, .y = y};
+}
+
+int main()
+{
+    struct t t = add(1.0, 0x1p-100);
+    printf("x + y = %a (upward)\n", t.x);
+    printf("x + y = %a (downward)\n", t.x);
+}
